804f1a7a08d2f5f40e1fdc093558cbaf4807157d
Giancarlos Castillo <gc.c@outlook.com>
2020-06-28 22:04:54 -0400

    feat: adds validation pipes to create task

--- src/tasks/dto/get-task-filter-dto.ts
+++ src/tasks/dto/get-task-filter-dto.ts
@@ -1,3 +1,4 @@
+import { IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTaskFilterDto {
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Delete,
   Patch,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 
 import { Task, TaskStatus } from './tasks.model';
@@ -32,6 +34,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.taskService.createTask(createTaskDto);
   }
