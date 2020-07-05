0dbd59436668917d472755974c26d4283a226f9f
Giancarlos Castillo <gc.c@outlook.com>
2020-07-05 09:09:19 -0400

    chore: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { TasksService } from './tasks/tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [],
   providers: [],
 })
